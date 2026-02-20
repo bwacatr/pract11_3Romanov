731e3cb4bbbc39a36c226916d736a816ffa1a91d
cheese <93201811+bwacatr@users.noreply.github.com>
2026-02-20 12:50:49 +0500

    Add files via upload

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Robot[] robots = new Robot[3];
+                for (int i = 0; i < robots.Length; i++)
+                {
+                    robots[i] = new Robot();
+                }
+                robots[0].kollife = Convert.ToInt32(textBox1.Text);
+                robots[1].kollife = Convert.ToInt32(textBox2.Text);
+                robots[2].kollife = Convert.ToInt32(textBox3.Text);
+                int startLife = robots[0].kollife;
+                label1.Text = "Количество жизней робота1 в начале игры: " + robots[0].kollife;
+                label2.Text = "Количество жизней робота2 в начале игры: " + robots[1].kollife;
+                label3.Text = "Количество жизней робота3 в начале игры: " + robots[2].kollife;
+                robots[0].min(startLife);
+                robots[0].Kol(startLife, new Robot[] {robots[1], robots[2] });
+                label6.Text = "Количество жизней робота1 в конце игры: " + robots[0].GetLife();
+                label5.Text = "Количество жизней робота2 в конце игры: " + robots[1].GetLife();
+                label4.Text = "Количество жизней робота3 в конце игры: " + robots[2].GetLife();
+
+
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка ввода");
+                throw;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WinFormsApp1/Robot.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    class Robot
+    {
+     
+        public int kollife;
+
+        public int GetLife()
+        {
+            return kollife;
+        }
+
+        public void Kol(int start, Robot[] mod)
+        {
+            if (start <= 0)
+                return;
+            double diff = start - this.kollife;
+            double percent = (diff / (double)start) * 100;
+
+            if (percent >= 50)
+            {
+                foreach (var a in mod)
+                {
+                    a.kollife += 30;
+
+                }
+
+            }
+            else if (percent >= 30)
+            {
+                foreach (var a in mod)
+                {
+                    a.kollife += 20;
+                }
+            }
+        }
+
+        public void min(int a)
+        {
+            kollife = a;
+            kollife = (int)kollife / 2;
+        }
+
+    }
+
+}
